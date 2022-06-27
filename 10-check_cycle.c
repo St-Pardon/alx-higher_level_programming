6a6f814394fba481e00df1c7546ee963effeecc5
St-Pardon <okc4pardon@gmail.com>
2022-06-27 15:26:02 +0000

    task 10

---
+++ 10-check_cycle.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the list
+ * Return: int 0 if there is no circle and 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p2, *prev;
+
+	p2 = list;
+	prev = list;
+
+	while (list && p2 && p2->next)
+	{
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
