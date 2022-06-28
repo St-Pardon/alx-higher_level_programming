2f6b88fdb64ae9c33a905c781cebcff589e74798
St-Pardon <okc4pardon@gmail.com>
2022-06-28 14:28:12 +0000

    fixed style in task 3 and debugged task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(100):
     if i == 99:
         print(i)
     else:
-        print(f"{'0' + str(i) if i < 10 else i}", end=", ")
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")
