932e68e5c98ae06e14dfc25bb319c92a0ee4a995
St-Pardon <okc4pardon@gmail.com>
2022-06-30 16:21:52 +0000

    mandatory task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    line = 1
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for arg in range(len(sys.argv) - 1):
-        print("{}: {}".format(line, sys.argv[line]))
-        line += 1
+    line = len(sys.argv) - 1
+
+    if line == 0:
+        print("{} arguments.".format(line))
+    elif line == 1:
+        print("{} argument:".format(line))
+    else:
+        print("{} arguments:".format(line))
+
+    if line >= 1:
+        line = 0
+        for arg in sys.argv:
+            if line != 0:
+                print("{}: {}".format(line, arg))
+            line += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-result = 0
-for arg in sys.argv:
-    if arg != sys.argv[0]:
-        result += int(arg)
-print(result)
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+            print(result)
