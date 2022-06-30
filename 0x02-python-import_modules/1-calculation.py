0fc3410729a4fbefbd70cb1639dcd5198f8da32d
St-Pardon <okc4pardon@gmail.com>
2022-06-30 16:00:46 +0000

    fixed task 1 - 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as cal
-a = 10
-b = 5
-print(f"{a} + {b} = {cal.add(a, b)}")
-print(f"{a} - {b} = {cal.sub(a, b)}")
-print(f"{a} * {b} = {cal.mul(a, b)}")
-print(f"{a} / {b} = {cal.div(a, b)}")
+if __name__ == "__main__":
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-line = 1
-print(f"{len(sys.argv) - 1} arguments:")
-for arg in range(len(sys.argv) - 1):
-    print(f"{line}: {sys.argv[line]}")
-    line += 1
+if __name__ == "__main__":
+    import sys
+    line = 1
+    print(f"{len(sys.argv) - 1} arguments:")
+    for arg in range(len(sys.argv) - 1):
+        print(f"{line}: {sys.argv[line]}")
+        line += 1
