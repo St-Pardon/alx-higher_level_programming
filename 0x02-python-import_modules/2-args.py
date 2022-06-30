7d94e832c14013d90b9810feb47908280c97c644
St-Pardon <okc4pardon@gmail.com>
2022-06-30 16:10:17 +0000

    fixed pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     line = 1
-    print("{} arguments:".format(len(sys.argv) - 1)
+    print("{} arguments:".format(len(sys.argv) - 1))
     for arg in range(len(sys.argv) - 1):
-        print(f"{}: {}".format(line, sys.argv[line])
-        line += 1
+        print(f"{}: {}".format(line, sys.argv[line]))
+        lne += 1
