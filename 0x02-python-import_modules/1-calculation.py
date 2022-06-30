19c5d9b72e9796fb5d8988c336c46f9d43ef88a7
St-Pardon <okc4pardon@gmail.com>
2022-06-30 16:06:25 +0000

    fixed task 1 - 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1 as cal
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, cal.add(a, b)))
-    print("{} - {} = {}".format(a, b, cal.sub(a, b))
-    print("{} * {} = {}".format(a, b, cal.mul(a, b))
-    print("{} / {} = {}".format(a, b, cal.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b))
+    print("{} * {} = {}".format(a, b, mul(a, b))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     line = 1
-    print(f"{len(sys.argv) - 1} arguments:")
+    print("{} arguments:".format(len(sys.argv) - 1)
     for arg in range(len(sys.argv) - 1):
-        print(f"{line}: {sys.argv[line]}")
+        print(f"{}: {}".format(line, sys.argv[line])
         line += 1
