aca462354c2f880321741a49f2f21139b43b9943
St-Pardon <okc4pardon@gmail.com>
2022-07-07 14:59:41 +0000

    last advanced

---
+++ 0x04-python-more_data_structures/#1-search_replace.py#
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    arr = my_list[:]
+    for i in arr:
+        if i == search:
+            arr[arr.index(i)] = replace
+    return arr
--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,1 +1,68 @@
 #include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_bytes - Prints bytes information
+ * @p: Python Object param
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	char *str;
+	long int size, i, n;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+
+	if (size >= 10)
+		n = 10;
+	else
+		n = size + 1;
+
+	printf("  first %ld bytes:", n);
+
+	for (i = 0; i < n; i++)
+		if (str[i] >= 0)
+			printf(" %02x", str[i]);
+		else
+			printf(" %02x", 256 + str[i]);
+
+	printf("\n");
+}
+
+/**
+ * print_python_list - Prints list information
+ * @p: Python Object param
+ */
+
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *arr;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	arr = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", arr->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
+}
