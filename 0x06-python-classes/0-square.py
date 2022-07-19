7cdbf54402361ff009208f992748ea77ab330dd8
St-Pardon <okc4pardon@gmail.com>
2022-07-19 19:52:27 +0000

    task 0 debug

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+A module for working with squares.
+"""
+
+
 class Square:
+    """
+    This would represents a 2D Polygon
+    with 4 equal and perpendicular sides.
+    """
     pass
