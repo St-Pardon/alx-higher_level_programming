5161d8cbdb0047ab600f98905ee5206e3020140d
St-Pardon <okc4pardon@gmail.com>
2022-07-19 19:35:36 +0000

    adanced 3-4

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def __eq__(self, value):
+        if isinstance(value, Square):
+            return self.area() == value.area()
+        else:
+            return False
+
+    def __ne__(self, value):
+        if isinstance(value, Square):
+            return self.area() != value.area()
+        else:
+            return True
+
+    def __gt__(self, value):
+        if isinstance(value, Square):
+            return self.area() > value.area()
+        else:
+            err_msg = "'>' not supported between instances of 'Square' and"
+            val_type = str(type(value)).split("'")[1]
+            raise TypeError("{} '{}'".format(err_msg, val_type))
+            return False
+
+    def __ge__(self, value):
+        if isinstance(value, Square):
+            return self.area() >= value.area()
+        else:
+            err_msg = "'>=' not supported between instances of 'Square' and"
+            val_type = str(type(value)).split("'")[1]
+            raise TypeError("{} '{}'".format(err_msg, val_type))
+            return False
+
+    def __lt__(self, value):
+        if isinstance(value, Square):
+            return self.area() < value.area()
+        else:
+            err_msg = "'<' not supported between instances of 'Square' and"
+            val_type = str(type(value)).split("'")[1]
+            raise TypeError("{} '{}'".format(err_msg, val_type))
+            return False
+
+    def __le__(self, value):
+        if isinstance(value, Square):
+            return self.area() <= value.area()
+        else:
+            err_msg = "'<=' not supported between instances of 'Square' and"
+            val_type = str(type(value)).split("'")[1]
+            raise TypeError("{} '{}'".format(err_msg, val_type))
+            return False
