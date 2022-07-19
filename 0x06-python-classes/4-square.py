140838f98e2137930feef2482543bca5bb2e5f9b
St-Pardon <okc4pardon@gmail.com>
2022-07-19 19:01:03 +0000

    task 4 -5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    def area(self):
+        return self.size ** 2
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print('')
+        else:
+            for i in range(self.size):
+                print('{}'.format('#' * self.size))
