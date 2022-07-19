a8be5b9af6c65b9dd1e85ccb22ef9e0eb04748e7
St-Pardon <okc4pardon@gmail.com>
2022-07-19 18:56:55 +0000

    task 0 - 3

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        super().__init__()
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        super().__init__()
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
