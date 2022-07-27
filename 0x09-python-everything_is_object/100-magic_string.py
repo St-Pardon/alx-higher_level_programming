c34d70cec076494317ef3561ddd69f750820256e
St-Pardon <okc4pardon@gmail.com>
2022-07-27 19:24:28 +0000

    uncopied

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', Holberton')
+    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', BestSchool')
     return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
