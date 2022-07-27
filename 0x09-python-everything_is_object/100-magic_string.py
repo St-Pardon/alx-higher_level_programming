9f1f2082ccd5c4ad26e41455768370037b4a7fcb
St-Pardon <okc4pardon@gmail.com>
2022-07-27 19:21:44 +0000

    last of it

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
+    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', Holberton')
+    return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
