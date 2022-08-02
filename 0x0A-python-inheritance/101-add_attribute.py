25204086568ec799ab226e4dbe4235d93930e05a
St-Pardon <okc4pardon@gmail.com>
2022-08-02 19:10:10 +0000

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(obj, name, value):
+    """function that adds a new attribute to an object if it’s possible"""
     if ('__dict__' in dir(obj)) and (type(obj.__dict__) is dict):
         obj.__dict__[name] = value
     else:
