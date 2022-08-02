ad3a8f65464bd8075608780cdcb9ffac79c21543
St-Pardon <okc4pardon@gmail.com>
2022-08-02 17:39:45 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding='utf-8') as file:
+        for line in file.readlines():
+            print(line, end='')
