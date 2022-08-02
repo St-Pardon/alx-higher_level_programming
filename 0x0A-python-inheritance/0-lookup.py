8479a2e3986896b5628481ef8b31b210aabd65cd
St-Pardon <okc4pardon@gmail.com>
2022-08-02 14:15:08 +0000

    task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    function that returns the list of available attributes and \
+    methods of an object
+    """
     return list(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class MyList(list):
+    """Mylist class that inherites from list"""
+
+
+    def print_sorted(self):
+        """function that prints the list, but sorted"""
+        sorted_list = sorted(self)
+        print(sorted_list)
+        del sorted_list
