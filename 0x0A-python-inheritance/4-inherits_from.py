0c822dc3261c4c8f99092025a5233c540ce6e203
St-Pardon <okc4pardon@gmail.com>
2022-08-02 15:56:06 +0000

    task 5 - 6

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """
+    object is an instance of a class that inherited (directly or indirectly) /
+    from the specified class
+    """
+    return issubclass(obj.__class__, a_class) and (type(obj) is not a_class)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
