b5419b4d6cb0c8a170fb54c98fcd1cafdb461102
St-Pardon <okc4pardon@gmail.com>
2022-08-08 16:04:04 +0000

    last off mandatory

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """The base Model"""
-
+from json import JSONDecoder, JSONEncoder
+import os
+import re
+from random import randint
 
 class Base:
     '''base class'''
@@ -12,3 +15,112 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Creates the JSON representation"""
+        if list_dictionaries is None:
+            return "[]"
+        return JSONEncoder().encode(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves a list of polygons to a file in JSON format"""
+        file_name = '{}.json'.format(cls.__name__)
+        dict_list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                if type(obj) is cls:
+                    dict_list.append(obj.to_dictionary())
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            file.write(Base.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Creates a list from its JSON representation."""
+        if (json_string is None) or (len(json_string.strip()) == 0):
+            return []
+        else:
+            return JSONDecoder().decode(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Creates a polygon with the given attributes."""
+        polygons = {
+            'Rectangle': (1, 1, 0, 0, None),
+            'Square': (1, 0, 0, None),
+        }
+        if cls.__name__ in polygons.keys():
+            polygon = cls(*polygons[cls.__name__])
+            polygon.update(**dictionary)
+            return polygon
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads a list of polygons from a file in JSON format."""
+        file_name = '{}.json'.format(cls.__name__)
+        lines = []
+        if os.path.isfile(file_name):
+            with open(file_name, mode='r') as file:
+                for line in file.readlines():
+                    lines.append(line)
+        txt = ''.join(lines)
+        attr_dicts = cls.from_json_string(txt)
+        cls_list = list(map(lambda x: cls.create(**x), attr_dicts))
+        return cls_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Saves a list of polygons to a file in CSV format."""
+        file_name = '{}.csv'.format(cls.__name__)
+        poly_fmt_fxns = {
+            'Rectangle': lambda x: '{},{:d},{:d},{:d},{:d}'.format(
+                x.id, x.width, x.height, x.x, x.y),
+            'Square': lambda x: '{},{:d},{:d},{:d}'.format(
+                x.id, x.size, x.x, x.y),
+        }
+        vals_list = []
+        if list_objs is not None:
+            poly_name = cls.__name__
+            for obj in list_objs:
+                if (type(obj) is cls) and (poly_name in poly_fmt_fxns):
+                    vals_list.append('{}\n'.format(
+                        poly_fmt_fxns[poly_name](obj)))
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            file.writelines(vals_list)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Loads a list of polygons from a file in CSV format."""
+        poly_name = cls.__name__
+        file_name = '{}.csv'.format(poly_name)
+        poly_fmt_fxns = {
+            'Rectangle': lambda x: {
+                'id': int(x[0]),
+                'width': int(x[1]),
+                'height': int(x[2]),
+                'x': int(x[3]),
+                'y': int(x[4]),
+            },
+            'Square': lambda x: {
+                'id': int(x[0]),
+                'size': int(x[1]),
+                'x': int(x[2]),
+                'y': int(x[3]),
+            },
+        }
+        poly_fmt = {
+            'Rectangle': r'\s*[^,]+,[^,]+,[^,]+,[^,]+,[^,]+',
+            'Square': r'\s*[^,]+,[^,]+,[^,]+,[^,]+',
+        }
+        lines = []
+        attr_dicts = []
+        if os.path.isfile(file_name):
+            with open(file_name, mode='r') as file:
+                for line in file.readlines():
+                    attrs_match = re.match(poly_fmt[poly_name], line)
+                    if attrs_match is not None:
+                        cols = line.strip().split(',')
+                        attr_dicts.append(poly_fmt_fxns[poly_name](cols))
+        cls_list = list(map(lambda x: cls.create(**x), attr_dicts))
+        return cls_list
