8bdb5f303b45f8dc62789bcbbaeb073611dca976
St-Pardon <okc4pardon@gmail.com>
2022-09-09 13:58:46 +0000

    fixed table creation

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXIST force_name(
+CREATE TABLE IF NOT EXISTS force_name(
        id INT,
        name VARCHAR(256) NOT NULL
 );
