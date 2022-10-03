a5d2f8e4b9a4c8249226746542e71ed5a6182898
St-Pardon <okc4pardon@gmail.com>
2022-10-03 21:02:20 +0000

    debug

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv[2]) {
-  console.log('Argument found');
+if (argv[3]) {
+  console.log('Arguments found');
+} else if (argv[2]) {
+    console.log('Argument found');
 } else {
   console.log('No argument');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 if (argv[2]) {
-  argv.filter((arg, i) => i > 1).forEach(val => console.log(val));
+  console.log(argv[2]);
 } else {
   console.log('No argument');
 }
