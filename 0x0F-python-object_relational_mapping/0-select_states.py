0a2cad6448d9f380a93baa4179ef6d8700d79322
St-Pardon <okc4pardon@gmail.com>
2022-10-08 09:56:58 +0000

    :tada: Task 0 - 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+connection = MySQLdb.connect(
+    host="localhost",
+    port=3306, user=argv[1],
+    passwd=argv[2],
+    db=argv[3],
+    charset="utf8"
+)
+db = connection.cursor()
+db.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = db.fetchall()
+
+for row in query_rows:
+    print(row)
+
+db.close()
+connection.close()
