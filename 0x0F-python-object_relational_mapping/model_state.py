9e408b28aba764bd843156d11330df3ad18ac804
St-Pardon <okc4pardon@gmail.com>
2022-10-08 17:18:39 +0000

    :heavy_check_mark: task 6 - 7

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''A module containing the State model.
+'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+'''Represents the base class for all tables.
+'''
+class State(Base):
+    '''Represents a row in a states table.
+    '''
+    __tablename__ = "states"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(length=128),
+        nullable=False
+    )
