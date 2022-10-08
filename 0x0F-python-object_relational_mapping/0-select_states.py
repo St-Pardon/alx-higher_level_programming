00511b44e6d08868dc3eb042305955ed9990fecd
St-Pardon <okc4pardon@gmail.com>
2022-10-08 10:19:51 +0000

    :wrench: Task 0 - 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Database connection"""
+"""
+Database connection
+"""
 from sys import argv
 import MySQLdb
 
-"""Connection set up"""
-connection = MySQLdb.connect(
-    host="localhost",
-    port=3306, user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    charset="utf8"
-)
-db = connection.cursor()
-db.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = db.fetchall()
+"""
+Connection set up
+using MySQLdb
+"""
+if __name__ == '__main__':
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    db = connection.cursor()
+    db.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = db.fetchall()
 
-for row in query_rows:
-    print(row)
+    for row in query_rows:
+        print(row)
 
-db.close()
-connection.close()
+    db.close()
+    connection.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,19 +4,20 @@ from sys import argv
 import MySQLdb
 
 '''connection Setup'''
-connection = MySQLdb.connect(
-    host="localhost",
-    port=3306, user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    charset="utf8"
-)
-db = connection.cursor()
-db.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = db.fetchall()
+if __name__ == '__main__':
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    db = connection.cursor()
+    db.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = db.fetchall()
 
-for row in query_rows:
-    print(row)
+    for row in query_rows:
+        print(row)
 
-db.close()
-connection.close()
+    db.close()
+    connection.close()
