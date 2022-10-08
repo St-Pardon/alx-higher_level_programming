d8373c871646e60f2503a2bcbe5b88d1612e1f90
St-Pardon <okc4pardon@gmail.com>
2022-10-08 15:30:57 +0000

    :white_check_mark: task 3 - 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''database connection script'''
+from sys import argv
+import MySQLdb
+
+'''connection Setup'''
+if __name__ == '__main__':
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    db = connection.cursor()
+    db.execute("SELECT cities.id, cities.name, states.name FROM cities" +
+               " INNER JOIN states ON cities.state_id = states.id" +
+               " ORDER BY cities.id ASC;")
+    query_rows = db.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    db.close()
+    connection.close()
