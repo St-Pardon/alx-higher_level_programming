e586d7a3d659582635302dce8dda2d04fc527c42
St-Pardon <okc4pardon@gmail.com>
2022-10-24 09:47:37 +0000

    :white_check_mark: task 3 - 4

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+'''
+from sys import argv
+from urllib import request, error
+
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        try:
+            with request.urlopen(argv[1]) as res:
+                print(res.read().decode('utf-8'))
+        except error.HTTPError as err:
+            print('Error code: {}'.format(err.code)}
