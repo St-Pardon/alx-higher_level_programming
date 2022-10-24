c4d3d5ff408e2a505b50d6d2952b50445df2f8fc
St-Pardon <okc4pardon@gmail.com>
2022-10-24 09:23:59 +0000

    :wrench: fixed pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ from urllib import request, parse
 
 if __name__ == '__main__':
     if len(argv) > 2:
-        data = parse.urlencode({'email' : argv[2]})
+        data = parse.urlencode({'email': argv[2]})
         data = data.encode('ascii')
         req = request.Request(argv[1], data)
         with request.urlopen(req) as res:
