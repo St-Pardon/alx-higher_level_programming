0ecae42270c7a5bf733856599775de145d8c0c76
St-Pardon <okc4pardon@gmail.com>
2022-10-24 09:18:06 +0000

    :wrench: fixed exec

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,12 @@
 Python script that fetches https://alx-intranet.hbtn.io/status
 '''
 import urllib.request as reqobj
-with reqobj.urlopen('https://alx-intranet.hbtn.io/status/') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    with reqobj.urlopen('https://alx-intranet.hbtn.io/status/') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
