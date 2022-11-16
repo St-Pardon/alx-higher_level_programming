a4dc1dd4337a334af67d1e4eb06d8aeccb02f43a
St-Pardon <okc4pardon@gmail.com>
2022-11-16 18:21:37 +0000

    :wrench: fixed

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,11 +3,15 @@ const request = require('request');
 if (process.argv.length > 2) {
   request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
     if (err) console.log(err);
-    for (let i = 0; i < JSON.parse(body).characters.length; i++) {
-      request(JSON.parse(body).characters[i], (err, res, body) => {
+    // for (let i = 0; i < JSON.parse(body).characters.length; i++) {
+    const names = JSON.parse(body).characters.map(item => new Promise((resolve, reject) => {
+      request(item, (err, res, body) => {
         if (err) console.log(err);
-        console.log(JSON.parse(body).name);
+        resolve(JSON.parse(body).name);
       });
-    }
+    }));
+    Promise.all(names)
+      .then(item => console.log(item.join('\n')))
+      .catch(err => console.log(err));
   });
 }
